dd0690414e44002f18a152816a17ae03d85dec85
Wills Bithrey <wills@willsbithrey.com>
2014-12-14 17:46:28 +0000

    Initial Commit

---
+++ Repaint Manager/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Repaint_Manager
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScanAircraft = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnScanAircraft
+            // 
+            this.btnScanAircraft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnScanAircraft.Location = new System.Drawing.Point(12, 12);
+            this.btnScanAircraft.Name = "btnScanAircraft";
+            this.btnScanAircraft.Size = new System.Drawing.Size(185, 23);
+            this.btnScanAircraft.TabIndex = 0;
+            this.btnScanAircraft.Text = "Scan for Aircraft";
+            this.btnScanAircraft.UseVisualStyleBackColor = true;
+            this.btnScanAircraft.Click += new System.EventHandler(this.btnScanAircraft_Click);
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 134);
+            this.Controls.Add(this.btnScanAircraft);
+            this.Name = "frmMain";
+            this.Text = "Repaint Manager";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScanAircraft;
+    }
+}
+
---
+++ Repaint Manager/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repaint_Manager
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnScanAircraft_Click(object sender, EventArgs e)
+        {
+            scanAircraft();
+        }
+
+        private void scanAircraft()
+        {
+            // Get the aircarft folder
+            string simPath = @"C:\Dummy PREPAR3D";
+            string aircraftFolder = @"SimObjects\Airplanes";
+            string aircraftPath = Path.Combine(simPath, aircraftFolder);
+            
+            Console.WriteLine("Aircraft folder path:", aircraftPath);
+
+            // Iterate over each subfolder and read each aircraft.cfg
+            string[] aircaftDirectories = Directory.GetDirectories(aircraftPath);
+
+            foreach (string subdirectory in aircaftDirectories)
+                ProcessAircraft(subdirectory);
+        }
+
+        private void ProcessAircraft(string directory)
+        {
+            Console.WriteLine("Processing aircraft:", directory);
+
+            // Read the aircarft.cfg and find the sim name.
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files) {
+                Console.WriteLine(file);
+                installRepaint(file);
+            }  
+        }
+
+        private void installRepaint(string path)
+        {
+            // TODO: Add support for folders
+
+            // Create temporary directory for dealing with files
+            string tmpAppDir = Path.Combine(Path.GetTempPath(), @"Repaint Manager");
+            string tmpAircraftDir = Path.Combine(tmpAppDir, @"Repaints", Path.GetFileNameWithoutExtension(path));
+
+            // Clean the directory if it already exists
+            if (Directory.Exists(tmpAircraftDir))
+            {
+                Directory.Delete(tmpAircraftDir, true);
+            }
+
+            // Extract the repaint 
+            using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Read))
+            {
+                archive.ExtractToDirectory(tmpAircraftDir);
+            }
+
+            // Find the  files we need
+            var textureDir = getTextureDir(tmpAircraftDir);
+            // var readme = getReadme(tmpAircraftDir);
+        }
+
+        private string getTextureDir(string aircraftBaseDir)
+        {
+            string textureDir= recursiveDirSearch(aircraftBaseDir, @"texture.*");
+            Console.WriteLine("Found texture directory at " + textureDir);
+            return textureDir;
+        }
+
+        private void getReadme(string tmpAircraftDir)
+        {
+            //Directory.EnumerateFiles(tmpAircraftDir, @"readme", SearchOption.AllDirectories);
+        }
+
+        private string recursiveDirSearch(string path, string dirName)
+        {
+            var allDirs = Directory.GetDirectories(path);
+            var matchingDirs = Directory.GetDirectories(path, dirName);
+            var found = "";
+
+            if (matchingDirs.Length == 0)
+            {
+                // Search each subdir
+                foreach (string sd in allDirs)
+                {
+                    var result = recursiveDirSearch(sd, dirName);
+                    if (result != "")
+                    {
+                        found = result;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                found = matchingDirs[0];
+            }
+            
+            return found;
+        }
+    }
+}
